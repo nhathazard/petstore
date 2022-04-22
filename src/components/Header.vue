20a313e3059570a49280fd01574e8faa5bfcf696
nhathazard <93414257+nhathazard@users.noreply.github.com>
2022-04-22 09:41:02 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <header class="header flex justify-between bg-slate-100 items-center h-[50px] mt-[-60px]">
+      <h1 class="text text-green-500 text-2xl font-bold">Pet Store</h1>
+      <ul class="flex mr-[12px] ">
+          <li class="nav-item px-2"><a class="nav-link" href="#/">Home</a></li>
+          <li class="nav-item px-2"><a class="nav-link" href="#login">Sign in</a></li>
+          <li class="nav-item px-2"><a class="nav-link" href="#login">Sign up</a></li>
+      </ul>
+  </header>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name:'Header'
+}
+</script>
+
+<style scoped>
+ 
+</style>
